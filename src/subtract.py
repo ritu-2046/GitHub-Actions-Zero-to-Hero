bee073968d2d000ec74da9035496d3f75d00496f
Ritu Adhikari <rituadhikari46@gmail.com>
2024-07-29 09:39:02 -0400

    Create subtract.py

---
+++ src/subtract.py
@@ -0,0 +1,10 @@
+# app.py
+# This is a the addition functionality
+
+def subtract(a, b):
+    return a - b
+
+def test_subtract():
+    assert subtract(1, 2) == -1
+    assert subtract(1, -1) == 0
+    assert subtract(-1, -3) == 2
