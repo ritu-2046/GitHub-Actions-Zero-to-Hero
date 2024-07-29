2dab66230d3f6151189388061444a607a59b024e
Ritu Adhikari <rituadhikari46@gmail.com>
2024-07-29 09:32:01 -0400

    Update addition.py in original state

--- src/addition.py
+++ src/addition.py
@@ -7,12 +7,3 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
-
-def multiply(a, b):
-    return a * b
-
-def test_multiply():
-    assert multiply(1, 2) == 2
-    assert multiply(1, -1) == -1
-    assert multiply(-1, -2) == 2
-    assert multiply(1, 0) == 0
